027175ae80869685ea1d67284e5d3341bc9d8639
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-01 18:50:12 +0600

    golla

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-// const pool = new Pool({
-//   database: process.env.DATABASE_name,
-//   host: process.env.DATABASE_host,
-//   password: process.env.DATABASE_password,
-//   user: process.env.DATABASE_user,
-//   port: process.env.DATABASE_PORT,
-// });
-
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   console.log("No connnection string found");
