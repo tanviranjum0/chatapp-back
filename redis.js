9316d78377f79f940c364eb537c002a1a1918a2d
Tanvir Anjum <tanviranjum010@gmail.com>
2025-12-01 18:58:58 +0600

    hola

--- redis.js
+++ redis.js
@@ -1,27 +1,8 @@
 const Redis = require("ioredis");
 const redisClient = new Redis({
   username: "default",
-  password: "XK7KU8fC12v4TUENJTjGBCkJqcTKY75V",
-  host: "redis-13727.c1.ap-southeast-1-1.ec2.cloud.redislabs.com",
-  port: 13727,
+  password: REDIS_PASSWORD,
+  host: REDIS_HOST,
+  port: REDIS_PORT,
 });
 module.exports = redisClient;
-
-// XK7KU8fC12v4TUENJTjGBCkJqcTKY75V
-
-// const { createClient } = require("redis");
-
-// const client = createClient({
-//   username: "default",
-//   password: "XK7KU8fC12v4TUENJTjGBCkJqcTKY75V",
-//   socket: {
-//     host: "redis-13727.c1.ap-southeast-1-1.ec2.cloud.redislabs.com",
-//     port: 13727,
-//   },
-// });
-
-// client.on("error", (err) => console.log("Redis Client Error", err));
-
-// client.connect();
-
-// module.exports = client;
